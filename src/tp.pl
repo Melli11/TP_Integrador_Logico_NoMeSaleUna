11734f984f40a0820ccb62d095cc6a82e07e83fb
Melli11 <mingerez@gmail.com>
2022-08-09 19:44:53 -0300

    Empezando TP

---
+++ src/tp.pl
@@ -0,0 +1,35 @@
+
+% Punto 1
+
+% Predicados justificacion
+
+% profesion representa aquello en lo que trabaja o desea trabajar
+% las habilidades representan en qué son buenos 
+% los hobbies son aquellas actividades que le gustan
+
+
+profesion(bakunin,aviacion_militar).habilidades(bakunin,conductor).
+historialCriminal(bakunin,[robo_aeronaves,fraude,tenencia_cafeina]).
+
+profesion(ravachol,inteligencia_militar).
+habilidades(ravachol,tiro_al_blanco).
+hobbies(ravachol,[juegos_de_azar,ajedrez,tiro_al_blanco]). 
+
+profesion(rosaDubovsky,recolector_de_basura).
+profesion(rosaDubovsky,asesina).
+habilidades(rosaDubovsky,[constructor_de_puentes,mirar_Peppa_Pig]).
+hobbies(rosaDubovsky,[construir_puentes,mirar_Peppa_Pig,fisica_cuantica]).
+
+profesion(emmaGoldman,profesora_de_judo).
+profesion(emmaGoldman,cineasta).
+% habilidades(emmaGoldman,Habilidad):-
+%     habilidades(judithButler,Habilidad_Judith),
+%     habilidades(elisaBachofen,Habilidad_Eli).
+
+habilidades(judithButler,profesora_de_judo).
+habilidades(judithButler,inteligencia_militar).
+habilidades(judithButler,judo).
+
+
+
+
